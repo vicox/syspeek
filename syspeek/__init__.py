fd4b55e071c9d0733c8378dd90892e58f204370f
Marco Trevisan (Treviño) <mail@3v1n0.net>
2023-09-20 16:38:15 +0200

    syspeek: Explicitly set the required Gtk and AppIndicator versions

--- syspeek/__init__.py
+++ syspeek/__init__.py
@@ -42,6 +42,11 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
 
+import gi
+
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
+
 from gi.repository import Gtk
 from syspeek.indicator import *
 
